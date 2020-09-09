489ca214ade6d70360f8007e331dc8c464ad7604
MariaE <maria.k.ekstrom@gmail.com>
2020-09-09 14:28:08 +0200

    Try to solve Issue #8 with merged master

--- GUI.py
+++ GUI.py
@@ -36,10 +36,6 @@ class TOGO_UI(QMainWindow):
         self.updateButton = QPushButton('Uppdatera')
         self.generalLayout.addWidget(self.updateButton, 0, 3)
 
-        #self.tableWidget = QTableWidget()
-        #self.generalLayout.addWidget(self.tableWidget, 8, 0, 1, 3)
-
-
     def updateView(self, data):
         for i in range(0,len(self.stopList)):
             k=i*len(data[self.stopList[i]])
@@ -48,33 +44,14 @@ class TOGO_UI(QMainWindow):
                 if self.widgetListName[k+j] is None:
                     self.widgetListName[k+j]=QLabel(nextEntry.name)
                     self.generalLayout.addWidget(self.widgetListName[k + j], k + j + 2, 0)
-                    self.widgetListStopTrack[k+j]=QLabel(self.stopList[0]+", "+nextEntry.track)
+                    self.widgetListStopTrack[k+j]=QLabel(self.stopList[i]+", "+nextEntry.track)
                     self.generalLayout.addWidget(self.widgetListStopTrack[k+j], k + j + 2, 1)
                     self.widgetListDirection[k+j]=QLabel(nextEntry.direction)
                     self.generalLayout.addWidget(self.widgetListDirection[k+j], k + j + 2, 2)
-                    self.widgetListDeltatime[k+j]=QLabel(nextEntry.deltatime + 'min')
+                    self.widgetListDeltatime[k+j]=QLabel(nextEntry.deltatime + ' min')
                     self.generalLayout.addWidget(self.widgetListDeltatime[k+j], k + j + 2, 3)
                 else:
                     self.widgetListName[k+j].setText(nextEntry.name)
-                    self.widgetListStopTrack[k + j].setText(self.stopList[0] + ", " + nextEntry.track)
+                    self.widgetListStopTrack[k + j].setText(self.stopList[i] + ", " + nextEntry.track)
                     self.widgetListDirection[k + j].setText(nextEntry.direction)
-                    self.widgetListDeltatime[k + j].setText(nextEntry.deltatime + 'min')
-
-    def createTable(self, data):
-        # Create table - not used at the moment
-        self.tableWidget.setRowCount(len(data['Lantmilsgatan'] )+ 1)
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setItem(0, 0, QTableWidgetItem("Avgång"))
-        self.tableWidget.setItem(0, 1, QTableWidgetItem("Mot"))
-        self.tableWidget.setItem(0, 2, QTableWidgetItem("Tid"))
-
-
-
-        for i in range(1, len(data['Lantmilsgatan']) + 1):
-            nextDeparture = data['Lantmilsgatan'][i-1]
-            self.tableWidget.setItem(i, 0, QTableWidgetItem(nextDeparture.name))
-            self.tableWidget.setItem(i, 1, QTableWidgetItem(nextDeparture.direction))
-            self.tableWidget.setItem(i, 2, QTableWidgetItem(nextDeparture.time.strftime("%Y-%m-%d %H:%M")))
-
-        self.tableWidget.move(0, 0)
-
+                    self.widgetListDeltatime[k + j].setText(nextEntry.deltatime + ' min')
